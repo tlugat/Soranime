605610cce8265c7755180a4c09225fb40fbc691d
imene <imene.benrach@gmail.com>
2020-04-13 17:29:15 +0200

    player

--- src/JavaScript/player.js
+++ src/JavaScript/player.js
@@ -1,18 +1,56 @@
-const audio = document.querySelector("audio");
-const video = document.querySelector("video");
-const play = document.querySelector(".play");
-const volumeUp = document.querySelector(".volume-up");
-const volumeDown = document.querySelector(".volume-down");
-const move = document.querySelector(".move");
-const back = document.querySelector(".back");
+const volumeUp = document.querySelector(".buttons__volume-up");
+const volumeDown = document.querySelector(".buttons__volume-down");
+const video = document.querySelector('.allVid__video');
+const juice = document.querySelector('.allVid__controls__color-juice');
+const btn = document.getElementById('buttons__play-pause');
+const orangeBar = document.querySelector('.allVid__controls__color-bar');
+const move = document.querySelector(".buttons__move");
+const back = document.querySelector(".buttons__back");
 
-play.addEventListener("click", () => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-});
+
+// play pause 
+function togglePlayPause() {
+
+    if(video.paused){
+        btn.className="pause";
+        video.play();
+    }
+    else {
+        btn.className = "play";
+        video.pause();
+    }
+
+}
+
+btn.onclick = function(){
+    togglePlayPause();
+}
+
+video.onclick = function() {
+  togglePlayPause();
+} 
+
+
+
+// color bar
+
+video.addEventListener('timeupdate', function(){
+
+    let juicePos = video.currentTime / video.duration;
+
+    juice.style.width = juicePos * 100 + '%';
+
+    if(video.ended) {
+        btn.className ="play";
+    }
+
+
+})
+
+
+
+
+// Volume
 
 volumeDown.addEventListener("click", () => {
   if (video.volume - 0.1 <= 0) {
@@ -31,6 +69,14 @@ volumeUp.addEventListener("click", () => {
   }
   console.log(video.volume);
 });
+
+
+// +/- 10sec
+
+move.addEventListener("click", () => {
+  video.currentTime += 10;
+});
+
 
 move.addEventListener("click", () => {
   video.currentTime += 10;
