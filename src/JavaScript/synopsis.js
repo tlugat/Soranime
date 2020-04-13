f046a9b0203547937e4a670e7a711e88daf9763a
Kylian Mendy <kylianmendy@MacBook-Pro-de-Kylian.home>
2020-04-13 16:44:25 +0200

    add synopsis function hompage

--- src/JavaScript/synopsis.js
+++ src/JavaScript/synopsis.js
@@ -1,15 +1,21 @@
 
 //contenu au click
-let synopsis = document.getElementById('synopsis');console.log('synopsis');
+let synopsis = document.getElementById('synopsis');
+console.log("synopsis");
 
 
 //image au click
 let film = document.getElementById('film');
 console.log('film');
 
+let cross = document.getElementById('cross');
+console.log('cross');
 
+//fermeture au click avec la croix
 film.addEventListener('click', function(){
   synopsis.classList.toggle("close");
 });
 
-
+cross.addEventListener('click', function(){
+  synopsis.classList.add("close");
+});
