cc2e035ff161e87afacacfb47438cb44a4439919
Teo Lugat <teo.lugat@hetic.net>
2020-04-14 16:11:32 +0200

    addd

--- src/JavaScript/synopsis.js
+++ src/JavaScript/synopsis.js
@@ -1,21 +1,19 @@
-
 //contenu au click
-let synopsis = document.getElementById('synopsis');
+let synopsis = document.getElementById("synopsis");
 console.log("synopsis");
 
-
 //image au click
-let film = document.getElementById('film');
-console.log('film');
+let film = document.getElementById("film");
+console.log("film");
 
-let cross = document.getElementById('cross');
-console.log('cross');
+let cross = document.getElementById("cross");
+console.log("cross");
 
 //fermeture au click avec la croix
-film.addEventListener('click', function(){
+film.addEventListener("click", function() {
   synopsis.classList.toggle("close");
 });
 
-cross.addEventListener('click', function(){
+cross.addEventListener("click", function() {
   synopsis.classList.add("close");
 });
