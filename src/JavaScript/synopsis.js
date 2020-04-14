af63b3cda0fbaf5db320f71f7bffb36795348ee1
Teo Lugat <teo.lugat@hetic.net>
2020-04-14 19:55:46 +0200

    synopsis news

--- src/JavaScript/synopsis.js
+++ src/JavaScript/synopsis.js
@@ -13,61 +13,53 @@ const $codeGeass = document.querySelector(".codeGeass");
 const $samuraiChamploo = document.querySelector(".samuraiChamploo");
 const $chateauAmbulant = document.querySelector(".chateauAmbulant");
 const $princesseMononoke = document.querySelector(".princesseMononoke");
+const $fate = document.querySelector(".fate");
+const $loveIsWar = document.querySelector(".loveIsWar");
+const $demonSlayer = document.querySelector(".demonSlayer");
+const $sao = document.querySelector(".sao");
 
-const synopsisCodeGeass = {
-  title: "Code Geass",
+// Objets pour fiches films
+const synopsisFate = {
+  title: "Fate Stay Night",
   synopsis:
-    "Dans la défaite, le Japon perdit son indépendance et devint la 11ème colonie de l'Empire de Britannia. ... Dans ce contexte géopolitique et social explosif, des groupuscules nationalistes japonais luttent pour redonner l'indépendance à leur pays.",
-  url: "assets/img/Homepage/Top10/Code_geass.png",
+    "Depuis des millénaires, la guerre secrète pour la possession du Saint-Graal se poursuit, connue de seuls quelques élus. Rin, une jeune mage, s'y prépare en invoquant son servant Archer, l'incarnation physique et spirituelle d'un héros légendaire qui combattra à ses côtés.",
+};
+const synopsisLoveIsWar = {
+  title: "Love is war",
+  synopsis:
+    "L'histoire suit le quotidien de Kaguya Shinomiya et de Miyuki Shirogane qui se cherchent mutuellement à faire avouer les sentiments de l'un pour l'autre.",
+};
+const synopsisDemonSlayer = {
+  title: "Demon slayer",
+  synopsis:
+    "Depuis les temps anciens, il existe des rumeurs concernant des démons mangeurs d'hommes qui se cachent dans les bois. Pour cette raison, les citadins locaux ne s'y aventurent jamais la nuit. La légende raconte aussi qu'un tueur déambule la nuit, chassant ces démons assoiffés de sang. Depuis la mort de son père, Tanjirou a pris sur lui pour subvenir aux besoins de sa famille. Malgré cette tragédie, ils réussissent à trouver un peu de bonheur au quotidien. Cependant, ce peu de bonheur se retrouve détruit le jour où Tanjirou découvre que sa famille s'est fait massacrer et que la seule survivante, sa sœur Nezuko, est devenue un démon. À sa grande surprise, Nezuko montre encore des signes d'émotion et de pensées humaines. Ainsi, commence la dure tâche de Tanjirou, celle de combattre les démons et de faire redevenir sa sœur humaine.",
+};
+const synopsisSao = {
+  title: "Sword Art Online (S.A.O)",
+  synopsis:
+    "Après avoir été renvoyé dans le monde réel, Kirito apprend que 300 joueurs de SAO, dont Asuna, ne se sont toujours pas réveillés. En suivant une piste indiquant la présence d'un personnage caché lui ressemblant beaucoup dans un autre VRMMORPG appelé ALfheim Online, Kirito décide d'entrer dans le jeu.",
 };
 
-const synopsisNaruto = {
-  title: "Naruto",
-  synopsis:
-    "Naruto est un garçon un peu spécial. Solitaire au caractère <br>fougueux, il n'est pas des plus appréciés dans son village. Malgré <br>cela, il garde au fond de lui une ambition: celle de devenir un<br> \"maître Hokage\", la plus haute distinction dans lordre des ninjas, <br> et ainsi obtenir la reconnaissance de ses pairs mais cela ne sera<br> pas de  tout repos ... Suivez l'éternel farceur dans sa quête du <br>secret de  sa naissance et de la conquête des fruits de son ambition !",
-};
+// Ouverture dy synopsis selon le film
 
-const synopsisSamuraiChamploo = {
-  title: "Samuraï Champloo",
-  synopsis:
-    "Une jeune fille, Fuu, recherche le samouraï qui sent le tournesol et se fait accompagner par deux individus originaux, Mugen l'extravagant, ancien pirate devenu vagabond, et Jin le samouraï (rōnin) impassible, à la suite d'un pari qu'elle a « gagné » contre ces deux derniers qui allaient s'entre-tuer.",
-};
-
-const synopsisChateauAmbulant = {
-  title: "Le château ambulant",
-  synopsis:
-    "Sophie, une orpheline de 18 ans, travaille dur dans la boutique de chapelier que lui a laissée son père. Un jour, en ville, elle croise Hauru, un magicien très séduisant mais faible de caractère. Une sorcière, se méprenant sur leurs sentiments, change Sophie en une vieille femme de 90 ans. Désespérée, Sophie erre dans la campagne quand elle aperçoit un étrange château qui se déplace sur d'immenses pattes.",
-};
-
-const synopsisPrincesseMononoke = {
-  title: "Princesse Mononoke",
-  synopsis:
-    "Ashitaka, un jeune guerrier japonais, affronte un sanglier géant et furieux qui attaque son village. Il tue la bête, mais se retrouve atteint par un mal mystérieux. Sur le conseil des sages, il part vers l'Ouest, à la recherche de ce qui a transformé l'animal en démon. Au cours de son périple, il rencontre San, une jeune fille qui vit avec les loups. Ashitaka apprend que les humains sont à l'origine de tous ces maux, car ils détruisent la forêt, qu'ils exploitent pour alimenter leurs forges.",
-};
-
-$codeGeass.addEventListener("click", function() {
+// synopsis des nouveautés
+$fate.addEventListener("click", function() {
   $synopsis.style.display = "flex";
-  $img.setAttribute("src", synopsisCodeGeass.url);
-  $synopsisTitle.textContent = synopsisCodeGeass.title;
-  $synopsisText.textContent = synopsisCodeGeass.synopsis;
+  $synopsisTitle.textContent = synopsisFate.title;
+  $synopsisText.textContent = synopsisFate.synopsis;
 });
-$naruto.addEventListener("click", function() {
+$loveIsWar.addEventListener("click", function() {
   $synopsis.style.display = "flex";
-  $synopsisTitle.textContent = synopsisNaruto.title;
-  $synopsisText.textContent = synopsisNaruto.synopsis;
+  $synopsisTitle.textContent = synopsisLoveIsWar.title;
+  $synopsisText.textContent = synopsisLoveIsWar.synopsis;
 });
-$samuraiChamploo.addEventListener("click", function() {
+$demonSlayer.addEventListener("click", function() {
   $synopsis.style.display = "flex";
-  $synopsisTitle.textContent = synopsisSamuraiChamploo.title;
-  $synopsisText.textContent = synopsisSamuraiChamploo.synopsis;
+  $synopsisTitle.textContent = synopsisDemonSlayer.title;
+  $synopsisText.textContent = synopsisDemonSlayer.synopsis;
 });
-$chateauAmbulant.addEventListener("click", function() {
+$sao.addEventListener("click", function() {
   $synopsis.style.display = "flex";
-  $synopsisTitle.textContent = synopsisChateauAmbulant.title;
-  $synopsisText.textContent = synopsisChateauAmbulant.synopsis;
-});
-$princesseMononoke.addEventListener("click", function() {
-  $synopsis.style.display = "flex";
-  $synopsisTitle.textContent = synopsisPrincesseMononoke.title;
-  $synopsisText.textContent = synopsisPrincesseMononoke.synopsis;
+  $synopsisTitle.textContent = synopsisSao.title;
+  $synopsisText.textContent = synopsisSao.synopsis;
 });
