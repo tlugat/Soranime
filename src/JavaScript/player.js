180c6c7c3bac9ca03e9fbdf67d95df19036bf2b4
Michael vieira <mike@macbook-pro-de-michael.home>
2020-04-14 22:54:53 +0200

    volumeslider

--- src/JavaScript/player.js
+++ src/JavaScript/player.js
@@ -5,6 +5,7 @@ const volumeDown = document.querySelector(".buttons__volume-down");
 const video = document.querySelector('.allVid__video');
 const juice = document.querySelector('.allVid__controls__color-juice');
 const btn = document.getElementById('buttons__play-pause');
+const volumeSlider = document.querySelector('.buttons__volume');
 const orangeBar = document.querySelector('.allVid__controls__color-bar');
 const move = document.querySelector(".buttons__move");
 const back = document.querySelector(".buttons__back");
@@ -57,7 +58,7 @@ video.addEventListener('timeupdate', function(){
 
 // Volume
 
-volumeDown.addEventListener("click", () => {
+/* volumeDown.addEventListener("click", () => {
   if (video.volume - 0.1 <= 0) {
     video.volume = 0;
   } else {
@@ -73,7 +74,16 @@ volumeUp.addEventListener("click", () => {
     video.volume += 0.1;
   }
   console.log(video.volume);
-});
+}); */
+
+
+
+volumeSlider.addEventListener('change', function(){
+
+
+  video.volume = volumeSlider.value / 100;
+
+})
 
 
 // +/- 10sec
